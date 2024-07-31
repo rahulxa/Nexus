f763e8af4e3942f36f3e4393a807abcf24773d6a
rahulxa <rahulac.5000@gmail.com>
2024-07-31 02:04:45 -0400

    Initialize Baackend

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, // Allow credentials (cookies)
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser()); //cookies for storing access and refresh tokens
+
+export { app }
---
+++ backend/src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL)
+    } catch (error) {
+        console.log("ERROR:", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
