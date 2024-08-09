857feba1382e96049a5fa0f5354e3033760f656e
rahulxa <rahulac.5000@gmail.com>
2024-08-09 02:50:23 -0400

    complete socket chat messages

--- backend/src/socket/socketManager.js
+++ backend/src/socket/socketManager.js
@@ -32,7 +32,26 @@ const connectToSocket = (server) => {
         });
 
         socket.on("chat-message", (data, sender) => {
+            const [matchingRoom, found] = Object.entries(connections)
+                .reduce(([room, isFound], [roomKey, roomValue]) => {
+                    if (!isFound && roomValue.includes(socket.id)) {
+                        return [roomKey, true]
+                    }
 
+                    return [room, isFound]
+                }, ["", false]);
+
+            if (found === true) {
+                if (messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = []
+                }
+                messages[matchingRoom].push({ 'sender': sender, 'data': data, "socket-id-sender": socket.id });
+                console.log("message:", key, ":", sender, data)
+
+                connections[matchingRoom].forEach(elem => {
+                    io.to(elem).emit("chat-message", data, sender, socket.id)
+                })
+            }
         });
 
         socket.on("disconnect", () => {
