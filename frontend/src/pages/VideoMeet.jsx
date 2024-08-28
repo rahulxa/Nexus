5c86639d0544b1ff5c9416804b20cb601326c923
rahulxa <rahulac.5000@gmail.com>
2024-08-28 17:39:13 -0400

    complete get message from server functionality

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -103,8 +103,33 @@ function VideoMeet() {
         }
     }, [audio, video])
 
-    const gotMessageFromServer = (fromId, message) => { }
+
     const addMessage = () => { }
+
+    const gotMessageFromServer = (fromId, message) => {
+        const signal = JSON.parse(message);
+        if (id !== socketIdRef.current) {
+            if (signal.sdp) {
+                connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)
+                    .then(() => {
+                        if (signal.sdp.type === "offer") {
+                            connections[fromId].createAnswer()
+                                .then((description) => {
+                                    connections[fromId].setLocalDescription(description)
+                                        .then(() => {
+                                            socketRef.current.emit("signal", fromId, JSON.stringify({ "sdp": connections[fromId].localDescription }))
+                                        }).catch(e => console.log(e))
+                                }).catch(e => console.log(e))
+                        }
+                    }).catch(e => console.log(e))
+                )
+                if (signal.ice) {
+                    connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice))
+                        .catch(e => console.log(e))
+                }
+            }
+        }
+    }
 
     const connectToSocketServer = () => {
         socketRef.current = io.connect(serverUrl, { secure: false });
